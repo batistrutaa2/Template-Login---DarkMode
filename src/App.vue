3d5697363d6bb6da888ed136f46aa64258bd6b33
Celso Romão <pepitobatista5@gmail.com>
2021-09-23 21:34:12 -0300

    First commit - Template Login :)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-screen h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-5" >
+    <div class="bg-white dark:bg-gray-800 shadow-xl w-full lg:w-4/12 md:w-8/12 sm:w-10/12 p-6">
+      <h4 class="font-semibold mb-3 border-b border-gray-200 dark:text-gray-200">login</h4>
+      <div class="space-y-3">
+        <input
+          class="px-3 py-2 placeholder-gray-300 border-gray-300 dark-border-800 w-full dark:bg-gray-600 dark:text-gray-200"
+          type="text"
+          placeholder="E-mail"
+        />
+
+        <input
+          class="px-3 py-2 placeholder-gray-300 border-gray-300 dark-border-800 w-full dark:bg-gray-600 dark:text-gray-200"
+          type="text"
+          placeholder="Senha"
+        />
+        <div class="text-right">
+          <button
+            class="
+              rounded
+              text-sm
+              px-3
+              py-2
+              bg-green-400
+              hover:bg-green-800
+              tex-green-900
+            "
+          >
+            login
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <DarkModeSwitch/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DarkModeSwitch from './components/DarkModeSwitch.vue';
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { DarkModeSwitch },
+  name: "App",
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/DarkModeSwitch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="absolute top-0 right-0 p-4 cursor-pointer dark:text-gray-300"
+    @click="toggle"
+  >
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DarkModeSwitch",
+  data() {
+    return {
+      isDark: false,
+    };
+  },
+
+  computed: {
+    name() {
+      return this.isDark ? "light" : "Dark";
+    },
+  },
+
+  watch: {
+    isDark(vl) {
+      const htmlel = document.getElementsByTagName("html")[0];
+      if (vl) {
+        htmlel.classList.add("dark");
+      } else {
+        htmlel.classList.remove("dark");
+      }
+    },
+  },
+
+  methods: {
+    toggle() {
+      this.isDark = !this.isDark;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
